d4c1735fd0967495f090162aa123208edefce711
yogurt <yusong1117.u@gmail.com>
2025-06-28 00:05:54 +0800

    [feat] run use SDL with mouse & keyboard

--- main.c
+++ main.c
@@ -33,7 +33,30 @@ static void lv_linux_disp_init(void)
     const int width = atoi(getenv("LV_SDL_VIDEO_WIDTH") ? : "800");
     const int height = atoi(getenv("LV_SDL_VIDEO_HEIGHT") ? : "480");
 
-    lv_sdl_window_create(width, height);
+    lv_group_set_default(lv_group_create());
+
+    lv_display_t * disp = lv_sdl_window_create(width, height);
+
+    lv_indev_t * mouse = lv_sdl_mouse_create();
+    lv_indev_set_group(mouse, lv_group_get_default());
+    lv_indev_set_display(mouse, disp);
+    lv_display_set_default(disp);
+
+    LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+    lv_obj_t * cursor_obj;
+    cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
+    lv_image_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
+    lv_indev_set_cursor(mouse, cursor_obj);           /*Connect the image  object to the driver*/
+
+    lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
+    lv_indev_set_display(mousewheel, disp);
+    lv_indev_set_group(mousewheel, lv_group_get_default());
+
+    lv_indev_t * kb = lv_sdl_keyboard_create();
+    lv_indev_set_display(kb, disp);
+    lv_indev_set_group(kb, lv_group_get_default());
+
+    // return disp;
 }
 #else
 #error Unsupported configuration
