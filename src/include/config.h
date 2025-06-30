57add151c61172028811b6aaf85b32f64f172eb4
yogurt <yusong1117.u@gmail.com>
2025-06-30 22:20:35 +0800

    [feat] init repo
    1. 显示框架支持 FB/SDL
    2. 支持交叉编译，musl_arm32, glibc_arm32
    3. 格式化源文件:
       git ls-files '*.c' '*.h' | xargs clang-format -i # 批量修改
       clang-format -i xxx.c # 单个文件

---
+++ src/include/config.h
@@ -0,0 +1,34 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+
+/************************** 屏幕相关 ******************************************/
+#define FB_DEV_NAME "/dev/fb0"
+#define H_RES 640 /* 水平分辨率 */
+#define V_RES 480 /* 垂直分辨率 */
+#define DISP_BUF_SIZE (H_RES * V_RES)
+
+/************************** INPUT EVENT 对应的文件路径 ************************/
+#define KEY_EVENT_PATH "/dev/input/event0"
+#define TOUCH_PANEL_EVENT_PATH "/dev/input/event1"
+#define POWER_KEY_EVENT_PATH "/dev/input/event2"
+
+/************************** 按键对应的 KEY CODE ******************************/
+#define AUDIO_KEY 2  /* 音频按键 */
+#define POWER_KEY 3  /* 电源按键 */
+#define CAMERA_KEY 4 /* 摄像头按键 */
+
+/************************** WIFI 列表展示的数量 *******************************/
+#define WIFI_LIST_SHOW_NUM 20
+
+/*****************************方正姚体加载路径**********************************/
+// #define ALI_PUHUITI_FONTPATH "/mnt/sd/output/fonts/Alibaba-PuHuiTi-Light.ttf"
+#define ALI_PUHUITI_FONTPATH "/usr/local/fonts/hei.TTF"
+#define ALI_PUHUITI_WEIGHT 32
+/* TP 最大的触摸点数 */
+#define MAX_COUNT 2
+
+#ifndef UNUSED
+#define UNUSED(x) ((void)(x))
+#endif
+
+#endif
